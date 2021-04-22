103a985d9d6f7586f9304a69438bd6d181ee8703
pipperandtyler <smurfing_for_dota@outlook.com>
2021-04-23 00:12:40 +0500

    This build contains the load testing script written in locust framework.

--- LoadTesting.py
+++ LoadTesting.py
@@ -7,8 +7,10 @@ class QuickstartUser(HttpUser):
 
     @task
     def hello_world(self):
-        self.client.get("https://staging.propcert.co.uk/admin/business-client-list")
-        self.client.get("https://staging.propcert.co.uk/admin/business-client-edit-detail/396171")
+        # self.client.get("https://staging.propcert.co.uk/admin/business-client-list")
+        # self.client.get("https://staging.propcert.co.uk/admin/business-client-edit-detail/396171")
+        self.client.get("example.com")
 
     def on_start(self):
-        self.client.post("https://staging.propcert.co.uk/my-admin", json={"username": "faisal", "password": "Dlogic123!"})
+        # self.client.post("https://staging.propcert.co.uk/my-admin", json={"username": "faisal", "password": "Dlogic123!"})
+        self.client.get("")
